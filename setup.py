faac3364920a43db9ecc14fe4d57d488d3565e06
Jaime <jaime@osmobot.com>
2019-01-16 16:04:29 -0800

    Stuff to make a python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='project_euler',
+    version='0.0.1',
+    author='Jaime',
+    author_email='jaimemarijke@gmail.com',
+    description='For fun implementations of ProjectEuler.net problems',
+    url='https://www.github.com/jaimemarijke/project-euler-solutions.git',
+    packages=find_packages(),
+    entry_points={},
+    install_requires=[],
+    include_package_data=True
+)
